fbb6795f93604355cb4396a834dc757b85db1661
Max <mpjoles@gmail.com>
2020-07-02 16:00:47 -0400

    changed phots
    added properly sized photos

--- themes.js
+++ themes.js
@@ -2,17 +2,17 @@
 var currentTime = new Date().getHours();
 console.log(currentTime);
 if (5 <= currentTime && currentTime < 11) {
-    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5e750966afbf2f3b3f235449/5ef63ac95f6d2b33ec7b3583_Morning_Image.png");
+    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5cc86f0e9b8ac6a99fddf645/5efb25d3cf4e50457ea99f18_mornning-cc.png");
     console.log("morning");
     console.log("testtt");
 }
 
 else if (11 <= currentTime && currentTime < 17) {
-    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5e750966afbf2f3b3f235449/5ef63d28bc7eb9242c3916fc_cc-day-image.png");
+    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5cc86f0e9b8ac6a99fddf645/5efb25954f1c9a59f14f771c_day-cc.png");
     console.log("afternoon");
 }
 
 else {
-    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5e750966afbf2f3b3f235449/5ef63ac9328bb853e3a9722b_night_Image.png");
+    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5cc86f0e9b8ac6a99fddf645/5efb25a8cf4e50e6b8a99e6b_night-cc.png");
     console.log("night");
 }
