622581d601f1c0649b8fedd8a9f5bba8ca0826df
Max <mpjoles@gmail.com>
2020-07-02 16:32:05 -0400

    removed attribute bug
    removed extra quotations on attribute selector causing an issue

--- themes.js
+++ themes.js
@@ -2,16 +2,16 @@
 var currentTime = new Date().getHours();
 console.log(currentTime);
 if (5 <= currentTime && currentTime < 11) {
-    console.log("morning");
-    $("[theme='dynamic']").forEach(target => target.classList.add('morning'));
+    console.log('morning');
+    $('[theme=dynamic]').forEach(target => target.classList.add('morning'));
 
 }
 
 else if (11 <= currentTime && currentTime < 17) {
-    console.log("afternoon");
+    console.log('afternoon');
 }
 
 else {
-    console.log("night");
-    $("[theme='dynamic']").forEach(target => target.classList.add('night'));
+    console.log('night');
+    $('[theme=dynamic]').forEach(target => target.classList.add('night'));
 }
