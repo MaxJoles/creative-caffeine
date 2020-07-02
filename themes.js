faabe0747f38f0ba0f20bec27c0ccc4439fabee8
Max <mpjoles@gmail.com>
2020-07-02 16:26:49 -0400

    changed from class selector to attribute selector
     Added and attribute called theme-dynamic to select for elements that need to be styled differently

--- themes.js
+++ themes.js
@@ -2,18 +2,16 @@
 var currentTime = new Date().getHours();
 console.log(currentTime);
 if (5 <= currentTime && currentTime < 11) {
-    $('.image-switch').forEach(target => target.classList.add('morning'));
     console.log("morning");
-    console.log("testtt");
+    $("[theme='dynamic']").forEach(target => target.classList.add('morning'));
+
 }
 
 else if (11 <= currentTime && currentTime < 17) {
-    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5cc86f0e9b8ac6a99fddf645/5efb25954f1c9a59f14f771c_day-cc.png");
-    $('.image-switch').attr("srcset", "https://uploads-ssl.webflow.com/5cc86f0e9b8ac6a99fddf645/5efb25954f1c9a59f14f771c_day-cc.png");
     console.log("afternoon");
 }
 
 else {
-    $('.image-switch').attr("src", "https://uploads-ssl.webflow.com/5cc86f0e9b8ac6a99fddf645/5efb25a8cf4e50e6b8a99e6b_night-cc.png");
     console.log("night");
+    $("[theme='dynamic']").forEach(target => target.classList.add('night'));
 }
